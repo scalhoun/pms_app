05273938e9e942350ceb183173e7eba41b4e0a35
Shane Calhoun <shane@calhouncreations.com>
2011-09-30 10:37:56 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PmsApp::Application.config.secret_token = 'e481f49b09960d5ed490f8f3dcae40420ae84fdaa5f434bc91be0fda668283b283afedb75f4fa0555d56f32a017c35475e705589ca4e1f66515193870aaf1256'
